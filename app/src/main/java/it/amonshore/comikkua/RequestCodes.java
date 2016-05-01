96f2fb1ee9b3172d7e110db5dec535ff65ffd5b6
narsenico <not_amon-dev@yahoo.it>
2016-05-01 14:52:25 +0200

    Gestiti permessi per archiviazione Android M (A0064)

--- app/src/main/java/it/amonshore/comikkua/RequestCodes.java
+++ app/src/main/java/it/amonshore/comikkua/RequestCodes.java
@@ -9,6 +9,8 @@ public abstract class RequestCodes {
     public final static int EDIT_RELEASE_REQUEST = 2001;
     public final static int LOAD_IMAGE = 3001;
     public final static int CROP_IMAGE = 3010;
+    //i codici di richiesta per i permessi possono essere solo da 0 a 255
+    public final static int WRITE_EXTERNAL_STORAGE_PERMISSION_REQUEST = 101;
 
     //TODO completare con gli altri codici
 }
